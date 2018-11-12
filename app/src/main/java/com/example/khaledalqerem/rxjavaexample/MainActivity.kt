bcc96b82e1c2241ed44313ca39855a51ef6c6104
Khaled Qasem <khaled.qasem@harri.com>
2018-11-12 17:44:49 +0200

    Understanding observables

--- app/src/main/java/com/example/khaledalqerem/rxjavaexample/MainActivity.kt
+++ app/src/main/java/com/example/khaledalqerem/rxjavaexample/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import io.reactivex.Observable
+import io.reactivex.ObservableEmitter
+import io.reactivex.ObservableOnSubscribe
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableObserver
@@ -17,31 +19,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val disposableObserver = getObserver()
-
+        val notes = prepareNotes()
         val observable =
-//            Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-            Observable.range(1, 20)
-//                .repeat(4)
+            Observable.create<Note> { emitter -> notes.forEach { emitter.onNext(it) } }
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .filter { it % 2 == 0 }
-                .map { "$it is even number" }
                 .subscribeWith(disposableObserver)
+//        val observable =
+//            Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+//            Observable.range(1, 20)
+////                .repeat(4)
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .filter { it % 2 == 0 }
+//                .map { "$it is even number" }
+//                .subscribeWith(disposableObserver)
 
 
         compositeDisposable.add(observable)
     }
 
 
-    private fun getObserver(): DisposableObserver<String> {
-        return object : DisposableObserver<String>() {
+    private fun getObserver(): DisposableObserver<Note> {
+        return object : DisposableObserver<Note>() {
+            override fun onNext(t: Note) {
+                Log.d("MainActivity", "note = ${t.note}")
+            }
+
             override fun onComplete() {
                 Log.d("MainActivity", "onComplete === All items are emitted")
             }
 
-            override fun onNext(t: String) {
-                Log.d("MainActivity", "onNext === >> print $t")
-            }
 
             override fun onError(e: Throwable) {
                 Log.d("MainActivity", "onError ${e.message}")
